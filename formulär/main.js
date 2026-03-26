7ec19266eeb1ae90ca95032a2baeb0e66661a90a
Nikitabjorknert <nb222tx@student.lnu.se>
2026-03-26 18:47:21 +0100

    formulär

--- formulär/main.js
+++ formulär/main.js
@@ -11,3 +11,26 @@ console.log(behandling, datum, tid);
 showB.textContent = behandling;
 showD.textContent = datum;
 showT.textContent = tid;
+
+const sendBtn = document.getElementById('sendBtn');
+sendBtn.classList.add('inactive');
+
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+
+function required() {
+    const nameValue = nameInput.value.trim();
+    const emailValue = emailInput.value.trim();
+
+    if (nameValue !== "" && emailValue !== "") {
+    sendBtn.disabled = false;
+    sendBtn.classList.remove('inactive');
+    sendBtn.classList.add('active');
+    } else {
+        sendBtn.disabled = true;
+    }
+}
+
+nameInput.addEventListener('input', required);
+emailInput.addEventListener('input', required);
+
